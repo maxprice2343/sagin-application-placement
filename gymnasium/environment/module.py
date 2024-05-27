fbec9609735cd5f660804c0f2be494e1d2b33163
Maxwell Price <20916080@student.curtin.edu.au>
2024-05-27 15:13:21 +0800

    Added classes representing nodes and modules

---
+++ gymnasium/environment/module.py
@@ -0,0 +1,10 @@
+class Module:
+    """This class represents an activity module. It consists of instructions
+    that require processing. """
+    def __init__(self, num_instructions, memory_required, data_size):
+        # The number of instructions in the module
+        self.num_instructions = num_instructions
+        # The memory required by the module in order to execute, in Bytes
+        self.memory_required = memory_required
+        # The amount of data required by the node as input, in Bytes
+        self.data_size = data_size
---
+++ gymnasium/environment/node.py
@@ -0,0 +1,10 @@
+class Node:
+    """This class represents a network node. It is capable of processing
+    instructions."""
+    def __init__(self, processing_speed, bandwidth, memory):
+        # The processing speed of the node in Bytes Per Second
+        self.processing_speed = processing_speed
+        # The bandwidth available to the node in Bytes Per Second
+        self.bandwidth = bandwidth
+        # The memory of the node in Bytes
+        self.memory = memory
