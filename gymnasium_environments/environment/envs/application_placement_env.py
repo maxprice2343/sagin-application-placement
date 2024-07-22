3ec2a7f58b01d3b6b3da1596f3b9e6dd3088b016
Maxwell Price <20916080@student.curtin.edu.au>
2024-07-22 09:06:52 +0800

    Updated rendering

--- gymnasium_environments/environment/envs/application_placement_env.py
+++ gymnasium_environments/environment/envs/application_placement_env.py
@@ -76,28 +76,46 @@ class ApplicationPlacementEnv(gym.Env):
         # Node 2 Speed, Node 2 Bandwidth, Node 2 Memory
         # ....
         # -----------------------------------------------------------------------
+        self.num_modules = random.randint(
+            NUM_MODULES_LOWER_BOUND, NUM_MODULES_UPPER_BOUND
+        )
+        self.num_nodes = random.randint(
+            NUM_NODES_LOWER_BOUND, NUM_NODES_UPPER_BOUND
+        )
+        self.num_modules = 5
+        self.num_nodes = 2
         self.observation_space = gym.spaces.Dict(
             {
                 "modules" : gym.spaces.Box(
-                    low=np.array([NUM_MODULES_LOWER_BOUND, MODULE_SIZE_LOWER_BOUND,
+                    low=np.resize(np.array([0, MODULE_SIZE_LOWER_BOUND,
                                   MODULE_MEMORY_REQUIRED_LOWER_BOUND, MODULE_DATA_SIZE_LOWER_BOUND]),
-                    high=np.array([NUM_MODULES_UPPER_BOUND, MODULE_SIZE_UPPER_BOUND,
-                                   MODULE_MEMORY_REQUIRED_UPPER_BOUND, MODULE_DATA_SIZE_UPPER_BOUND])
+                                  (self.num_modules, 4)),
+                    high=np.resize(np.array([NUM_MODULES_UPPER_BOUND, MODULE_SIZE_UPPER_BOUND,
+                                   MODULE_MEMORY_REQUIRED_UPPER_BOUND, MODULE_DATA_SIZE_UPPER_BOUND]),
+                                   (self.num_modules, 4)),
+                    shape=(self.num_modules, 4),
+                    dtype=np.int64
                 ),
                 "nodes" : gym.spaces.Box(
-                    low=np.array([NUM_NODES_LOWER_BOUND, NODE_SPEED_LOWER_BOUND,
+                    low=np.resize(np.array([0, NODE_SPEED_LOWER_BOUND,
                                   NODE_BANDWIDTH_LOWER_BOUND, NODE_MEMORY_LOWER_BOUND]),
-                    high=np.array([NUM_NODES_UPPER_BOUND, NODE_SPEED_UPPER_BOUND,
-                                   NODE_BANDWIDTH_UPPER_BOUND, NODE_MEMORY_UPPER_BOUND])
+                                  (self.num_nodes, 4)),
+                    high=np.resize(np.array([NUM_NODES_UPPER_BOUND, NODE_SPEED_UPPER_BOUND,
+                                   NODE_BANDWIDTH_UPPER_BOUND, NODE_MEMORY_UPPER_BOUND]),
+                                   (self.num_nodes, 4)),
+                    shape=(self.num_nodes, 4),
+                    dtype=np.int64
                 )
             }
         )
         # The action taken by an agent will be placing a specific module on
         # a specific node for processing.
         self.action_space = gym.spaces.Box(
-            low = np.array([NUM_MODULES_LOWER_BOUND, NUM_MODULES_UPPER_BOUND]),
-            high = np.array([NUM_NODES_LOWER_BOUND, NUM_NODES_UPPER_BOUND])
+            low = np.array([NUM_MODULES_LOWER_BOUND, NUM_NODES_LOWER_BOUND]),
+            high = np.array([NUM_MODULES_UPPER_BOUND, NUM_NODES_UPPER_BOUND])
         )
+
+        self.window_size = 1024
 
         # Ensures the provided render mode is None (i.e. no rendering will happen)
         # or it is one of the accepted render methods
@@ -111,16 +129,14 @@ class ApplicationPlacementEnv(gym.Env):
         with random characteristics"""
         super().reset(seed=seed)
 
-        num_modules = random.randint(
-            NUM_MODULES_LOWER_BOUND, NUM_MODULES_UPPER_BOUND
-        )
-        self.modules = self._generate_modules(num_modules)
-        num_nodes = random.randint(
-            NUM_NODES_LOWER_BOUND, NUM_NODES_UPPER_BOUND
-        )
-        self.nodes = self._generate_nodes(num_nodes)
+        self.modules = self._generate_modules(self.num_modules)
+        self.nodes = self._generate_nodes(self.num_nodes)
 
-        return self._get_obs(), None
+        obs = self._get_obs()
+        """print(f"# Modules: {self.num_modules}")
+        print(f"# Nodes: {self.num_nodes}")
+        print(obs)"""
+        return obs, {}
     
     def step(self, action):
         """Moves the environment forward by 1 step (assigning a module to a
@@ -130,11 +146,12 @@ class ApplicationPlacementEnv(gym.Env):
 
         # Adding the module to the node's processing list
         node.add_module(action[0], module)
+        module.start_processing()
         
         # If any module is not yet finished processing then the environment
         # can't terminate yet
         terminated = True
-        for k, v in self.modules:
+        for k, v in self.modules.items():
             if not v.done:
                 terminated = False
                 break
@@ -144,7 +161,7 @@ class ApplicationPlacementEnv(gym.Env):
         # Calculates the memory already used by other modules deployed on the
         # node
         memory_used = 0
-        for k, v in node.modules:
+        for k, v in node.modules.items():
             if k != action[0]:
                 memory_used += v.memory_required
         # Calculates the resource overhead of the current module
@@ -165,7 +182,7 @@ class ApplicationPlacementEnv(gym.Env):
         """if self.render_mode == "human":
             self._render_frame()"""
 
-        return observation, reward, terminated, False, None
+        return observation, reward, terminated, False, {}
     
     def render(self):
         """Returns an rgb array representing the environment."""
@@ -179,14 +196,14 @@ class ApplicationPlacementEnv(gym.Env):
             pg.init()
             pg.display.init()
             pg.font.init()
-            self.text_font = pg.font.SysFont("Arial", 12)
-            self.window = pg.display.set_mode((512, 512))
+            self.text_font = pg.font.SysFont("Arial", 20)
+            self.window = pg.display.set_mode((self.window_size, self.window_size))
 
         if self.clock is None and self.render_mode == "human":
             self.clock = pg.time.Clock()
 
         # Creates the canvas with a white background to draw on
-        canvas = pg.Surface((512, 512))
+        canvas = pg.Surface((self.window_size, self.window_size))
         canvas.fill((255,255,255))
 
         #TODO: Add variables to store window size, etc.
@@ -195,21 +212,26 @@ class ApplicationPlacementEnv(gym.Env):
         canvas.blit(module_text_surface, (10,10))
 
         node_text_surface = self.text_font.render("Nodes:", False, (0,0,0))
-        canvas.blit(node_text_surface, (400,400))
+        canvas.blit(node_text_surface, (400,10))
 
         #TODO: Add labels to illustrations
 
-        for i, (k, v) in self.modules:
-            pg.draw.rect(canvas, (37,58,76), pg.Rect((20, 10 * i), (40, 20)))
+        for i, (k, v) in enumerate(self.modules.items()):
+            if v.processing == False:
+                pg.draw.circle(canvas, (37, 58, 76), (25, 40 * (k+1)), 10)
         
-        for i, (k, v) in self.nodes:
-            pg.draw.rect(canvas, (226,131,89), pg.Rect((400, 10 * i), (40, 20)))
+        for i, (k, v) in enumerate(self.nodes.items()):
+            pg.draw.rect(canvas, (226, 131, 89), pg.Rect((400, 40 * (i+1)), (40, 20)))
+            print(self.nodes[k].modules)
+            if len(self.nodes[k].modules) > 0:
+                for j, (kn, vn) in enumerate(self.nodes[k].modules.items()):
+                    pg.draw.circle(canvas, (37, 58, 76), (400 * (j+1) + 40 + 15, 45 * (i+1)), 10)
 
         if self.render_mode == "human":
             self.window.blit(canvas, canvas.get_rect()) # type: ignore
             pg.event.pump()
             pg.display.update()
-            self.clock.tick(self.metadata["framerate"]) # type: ignore
+            self.clock.tick(self.metadata["render_fps"]) # type: ignore
         else:
             return np.transpose(
                 np.array(pg.surfarray.pixels3d(canvas)), axes=(1,0,2)
@@ -223,10 +245,10 @@ class ApplicationPlacementEnv(gym.Env):
     def _get_obs(self):
         """Translates the environment's current state into an observation"""
         modules = np.ndarray(shape=(len(self.modules), 4), dtype=int)
-        for i, (k, v) in enumerate(self.modules):
+        for i, (k, v) in enumerate(self.modules.items()):
             modules[i] = [k, v.num_instructions, v.memory_required, v.data_size]
         nodes = np.ndarray(shape=(len(self.nodes), 4), dtype=int)
-        for i, (k, v) in enumerate(self.nodes):
+        for i, (k, v) in enumerate(self.nodes.items()):
             nodes[i] = [k, v.processing_speed, v.bandwidth, v.memory]
 
         return {
@@ -242,7 +264,7 @@ class ApplicationPlacementEnv(gym.Env):
             # Generates properties of the new modules randomly between set
             # bounds
             num_instructions = random.randint(
-                MODULE_SIZE_LOWER_BOUND, MODULE_DATA_SIZE_UPPER_BOUND
+                MODULE_SIZE_LOWER_BOUND, MODULE_SIZE_UPPER_BOUND
             )
             memory_required = random.randint(
                 MODULE_MEMORY_REQUIRED_LOWER_BOUND,
