0f766f6001e2fdac420b05e20bd29e35748f84eb
Maxwell Price <20916080@student.curtin.edu.au>
2024-07-22 09:06:43 +0800

    Added additional properties to module class to reflect it's state

--- gymnasium_environments/environment/application_module.py
+++ gymnasium_environments/environment/application_module.py
@@ -8,5 +8,14 @@ class Application_Module:
         self.memory_required = memory_required
         # The amount of data required by the node as input, in Bytes
         self.data_size = data_size
-        # Stores whether or not the processing for this node has been completed
+        # Stores whether or not the module is currently being processed
+        self.processing = False
+        # Stores whether or not the processing for this module has been completed
         self.done = False
+    
+    def start_processing(self):
+        self.processing = True
+    
+    def finish_processing(self):
+        self.processing = True
+        self.done = True
