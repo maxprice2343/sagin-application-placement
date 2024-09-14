580914212a5625c38c47154540cf5c4fcc550848
Maxwell Price <20916080@student.curtin.edu.au>
2024-09-14 15:00:47 +0800

    Updated Node class with asynchronous functions

--- gymnasium_environments/environment/network_node.py
+++ gymnasium_environments/environment/network_node.py
@@ -1,5 +1,5 @@
 from environment.application_module import Application_Module
-import queue
+from collections import deque
 import asyncio
 
 class Network_Node:
@@ -10,20 +10,34 @@ class Network_Node:
         self.processing_speed = processing_speed
         # The bandwidth available to the node in Bytes Per Second
         self.bandwidth = bandwidth
-        # The memory of the node in Bytes
-        self.memory = memory
+        # The total memory of the node in Bytes
+        self.total_memory = memory
+        # The available memory of the node in Bytes
+        self.available_memory = memory
 
-        self.modules = queue.Queue(max_modules)
+        self.modules = deque([], max_modules)
+
+        self.processing = False
     
     async def add_module(self, new_module : Application_Module) -> None:
         """Adds a new module into this node's queue of modules to be processed.
         Contains a loop that continuously processes modules in the queue until the
         queue is empty."""
 
-        self.modules.put(new_module)
+        if new_module.memory_required <= self.available_memory:
+            new_module.start_processing()
+            self.modules.appendleft(new_module)
+        
+        if not self.processing:
+            asyncio.create_task(self.process_modules())
 
-        while not self.modules.empty():
-            module_to_process = self.modules.get()
-            module_to_process.start_processing()
+    async def process_modules(self): #TODO: Modules are immediately removed from queue, causing them to not be shown on the render
+        self.processing = True
+        while len(self.modules) != 0:
+            module_to_process = self.modules[-1]
+            print(module_to_process.num_instructions / self.processing_speed)
             await asyncio.sleep(module_to_process.num_instructions / self.processing_speed)
             module_to_process.finish_processing()
+            self.modules.pop()
+            self.available_memory -= module_to_process.memory_required
+        self.processing = False
